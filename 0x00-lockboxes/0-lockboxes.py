2fd558c9f50ba581b7c860eb9967060184a95bc5
juankarlos999 <juan_karlos_999@hotmail.com>
2021-04-22 23:39:24 -0500

    [FIX]function Lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -15,19 +15,31 @@ def canUnlockAll(boxes):
     if can be opened: return True
     else: return False
     """
-    num_box = set(range(0, len(boxes)))
-    num_keys = set(boxes[0])
-    num_box_called = set({0})
-    '''
-    current box sublist index index box with closed key
-    '''
-    current_box = (num_keys - num_box_called).pop()
-    num_box_called.add(current_box)
-    try:
-        num_keys.update(boxes[current_box])
-    except:
-        pass
-    if len(num_box - num_box_called) == 0:
-        return True
-    else:
+    numBoxes = len(boxes)
+    if numBoxes == 0:
+        return false
+    openBoxes = [1]
+    keys = []
+    for i in range(1, numBoxes):
+        openBoxes.append(0)
+    for box in boxes[0]:
+        if box >= numBoxes:
+            continue
+        keys.append(box)
+        openBoxes[box] = 1
+    posBox = 1
+    while posBox < numBoxes:
+        if posBox in keys:
+            for key in boxes[posBox]:
+                if key >= numBoxes:
+                    continue
+                keys.append(key)
+                if openBoxes[key] == 0 and key < posBox:
+                    openBoxes[key] = 1
+                    posBox = key - 1
+                    break
+                openBoxes[key] = 1
+        posBox += 1
+    if 0 in openBoxes:
         return False
+    return True
