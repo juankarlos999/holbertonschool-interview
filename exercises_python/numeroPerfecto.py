d716e3c10d438a65c8fa2692e7c2585fe4578149
Juan R <55101788+juankarlos999@users.noreply.github.com>
2021-06-15 15:27:07 -0500

    add validation __name__

--- exercises_python/numeroPerfecto.py
+++ exercises_python/numeroPerfecto.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 '''
-    Funcion que reciba como entrada n números enteros positivos, y por cada uno
+    Funcion que recibe como entrada n números enteros positivos, y por cada uno
     de ellos imprime sus divisores e indica si es perfecto o no
 '''
 def numeroPerfecto(num):
@@ -17,16 +17,16 @@ def numeroPerfecto(num):
         print(f"suma de divisores = {suma}")
         return False
 
-# Entrada que indiqua el numero de pacientes que se realizaron el test
-numInputs = int(input("Introduzca la cantidad de numeros a validar: "))
-i = 1
-while i <= numInputs:
-    num = int(input("introduzca un numero: "))
-    # llamada al metodo numeroPerfecto
-    if numeroPerfecto(num):
-	    print(f"{num} es un numero perfecto")
-    else:
-	    print(f"{num} no es un numero perfecto")
+if __name__ == "__main__":
+	numInputs = int(input("Introduzca la cantidad de numeros a validar: "))
+	i = 1
+	while i <= numInputs:
+	    num = int(input("introduzca un numero: "))
+	    # llamada al metodo numeroPerfecto
+	    if numeroPerfecto(num):
+		    print(f"{num} es un numero perfecto")
+	    else:
+		    print(f"{num} no es un numero perfecto")
 
-    # incremento contador del bucle
-    i += 1
+	    # incremento contador del bucle
+	    i += 1
