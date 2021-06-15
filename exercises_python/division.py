26a168f2c451521d9ec02b5f6ee4f3c59025afbd
Juan R <55101788+juankarlos999@users.noreply.github.com>
2021-06-15 15:33:41 -0500

    pep8 style correction

--- exercises_python/division.py
+++ exercises_python/division.py
@@ -11,32 +11,32 @@
     Fin
 '''
 
-'''
-    Funcion que recibe como parametro dos numeros enteros y retorna la
-    division de ambos numeros
-'''
+
 def division(dividendo,divisor):
+    '''Funcion que recibe como parametro dos numeros enteros y retorna la
+    division de ambos numeros.
+    '''
     return dividendo/divisor
 
-'''
-    Funcion que llama a la funcion division para imprimir por pantalla el 
-    resultado
-'''
+
 def printDivision():
-    # inputs para variables a,b y llamado a la funcion division
+    '''Funcion que llama a la funcion division para imprimir por pantalla el 
+    resultado.
+    '''
     global dividendo
     global divisor
 
     dividendo = int(input("ingrese un numero entero para la variable dividendo: "))
     divisor = int(input("ingrese un numero entero para la variable divisor: "))
 
-    cociente = int(division(dividendo,divisor))
-    print("El cociente es de:",cociente)
+    cociente = int(division(dividendo, divisor))
+    print("El cociente es de:", cociente)
         
 
-# Declaracion e inicializacion de variables
-dividendo = 0
-divisor = 0
+if __name__ == "__main__":
+    # Declaracion e inicializacion de variables
+    dividendo = 0
+    divisor = 0
 
-# Llamado a la funcion printDivision
-printDivision()
+    # Llamado a la funcion printDivision
+    printDivision()
